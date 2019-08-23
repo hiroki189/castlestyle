bfdc098a6da68f733c0ecafedf02d6f8127a51c6
hiroki189 <hiroki18950@icloud.com>
2019-08-23 17:18:55 +0900

    modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'castle#index'
+  root 'castles#index'
+  resources :castles, only: [:new]
   resources :users, only: [:index, :edit, :new]
 end
