f5cf5100b63e8c68478a69220e48edf690b41941
hiroki189 <hiroki18950@icloud.com>
2019-08-27 18:36:31 +0900

    modify

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '13.114.92.47', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
