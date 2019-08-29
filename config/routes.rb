bab6239d253605e132b4b5893ec9f3c05d42a737
hiroki189 <hiroki18950@icloud.com>
2019-08-29 18:33:57 +0900

    routes users index action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'castles#index'
   resources :castles, only: [:new, :show]
+  resources :users, only: [:index]
 end
