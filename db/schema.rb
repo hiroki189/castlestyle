8a2e2bc2f9b4401ac4d6f2104d5dc0cb7fc82c81
hiroki189 <hiroki18950@icloud.com>
2019-08-29 18:35:13 +0900

    scheme modify

--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2019_08_28_143656) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", default: "", null: false
+    t.string "name", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
