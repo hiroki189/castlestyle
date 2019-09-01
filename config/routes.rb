474f14a0a88aad7f0c7b2892afd6a809a3fccc81
hiroki189 <hiroki18950@icloud.com>
2019-09-01 11:45:51 +0900

    route create update action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'castles#index'
-  resources :castles, only: [:new, :show]
+  resources :castles, only: [:new, :show,:create,:update]
   resources :users, only: [:index]
 end
