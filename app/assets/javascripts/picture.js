7ebb2581c5d20a2c228096057cab08ee01b72300
hiroki189 <hiroki18950@icloud.com>
2019-09-01 17:53:16 +0900

    create picture.js

---
+++ app/assets/javascripts/picture.js
@@ -0,0 +1,26 @@
+$(function(){
+  var itemNum = 1;
+  function buildImgHtml(blobUrl){
+    var image = `<img src="${blobUrl}" id="preview-image">`;
+    return image;
+  }
+
+  function appendImgBox(insertImg){
+    var imagePreviewHtml = '';
+    imagePreviewHtml = `<div class="style__photo__narrow__container__new__photo">
+                        ${insertImg}
+                        <input id="pic_upload" class="style__photo__narrow__container__new__photo__image" data-image= "${itemNum}" type="file" name="castle[image]">
+                        </div>
+                        <div class="style__photo__narrow__container__new__field">
+                        </div>`
+    $(".style__photo__narrow__container__new__field").prepend((imagePreviewHtml));
+  }
+
+  $(document).on("change", '#pic_upload', function(e){
+    var file = e.target.files[0];
+    var blobUrl = window.URL.createObjectURL(file);
+    insertImg = buildImgHtml(blobUrl);
+    $('.style__photo__narrow__container__new__photo').css('display', 'none');
+    appendImgBox(insertImg);
+  });
+});
