6dfb44f54539917bbdf2b5540700d22cf4f3d396
hiroki189 <hiroki18950@icloud.com>
2019-09-02 19:45:02 +0900

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_08_31_142147) do
 
   create_table "castles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
+    t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
