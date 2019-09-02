9d3bd545bfe78e17fd988dd7065ad233b66728db
hiroki189 <hiroki18950@icloud.com>
2019-09-02 19:55:59 +0900

    after posting redirect_to index

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -12,8 +12,11 @@ class CastlesController < ApplicationController
 
   def create
     Castle.create(castle_params)
+
+    redirect_to controller: :castles, action: :index
   end
 
+  private
   def castle_params
     params.require(:castle).permit(:name,:image, :content)
   end
