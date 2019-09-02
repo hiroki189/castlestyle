e9a096eac9ca3cdee046570853c75e824f7f78bf
hiroki189 <hiroki18950@icloud.com>
2019-09-02 19:44:53 +0900

    migrate castle content

--- db/migrate/20190822101847_create_castles.rb
+++ db/migrate/20190822101847_create_castles.rb
@@ -2,6 +2,7 @@ class CreateCastles < ActiveRecord::Migration[5.2]
   def change
     create_table :castles do |t|
       t.string :name, null: false
+      t.string :content, null: false
 
       t.timestamps
     end
