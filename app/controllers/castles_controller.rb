b86f7e3fe8a9383840c0ee7f7b20f80991b5e368
hiroki189 <hiroki18950@icloud.com>
2019-09-03 19:54:01 +0900

    castle controller

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -1,18 +1,19 @@
 class CastlesController < ApplicationController
   def index
+    @castle = Castle.all.limit(6)
   end
 
   def show
   end
 
   def new
+    redirect_to new_user_session_path unless user_signed_in?
     @castle = Castle.new
     # binding.pry
   end
 
   def create
     Castle.create(castle_params)
-
     redirect_to controller: :castles, action: :index
   end
 
