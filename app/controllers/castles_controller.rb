f605b3eba5c730e4b1313174401dc6d8e8791550
hiroki189 <hiroki18950@icloud.com>
2019-09-11 17:57:47 +0900

    castle controller

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -1,11 +1,13 @@
 class CastlesController < ApplicationController
-  before_action  :configure_title
   def index
-    @castles = Castle.all.limit(6)
+    @castles = Castle.order('id ASC').limit(6)
   end
 
   def show
     @castles = Castle.order("created_at DESC").page(params[:page]).per(4)
+    castle_ids = Castle.group(:citadel_id).order('count_citadel_id DESC').limit(5).count(:citadel_id).keys
+    # @ranking = castle_ids.map {|id| Castle.find(id)}
+    # binding.pry
   end
 
   def new
@@ -20,10 +22,7 @@ class CastlesController < ApplicationController
 
   private
   def castle_params
-    params.require(:castle).permit(:name,:castle_area,:image, :content)
+    params.require(:castle).permit(:name,:citadel_id,:image, :content)
   end
 
-  def configure_title
-    @page_title = '作品ページ'
-  end
 end
