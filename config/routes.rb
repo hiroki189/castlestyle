cd3401b5f80b06baf4659a462023f8c87d04fca1
hiroki189 <hiroki18950@icloud.com>
2019-09-11 17:59:16 +0900

    route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'castles#index'
-  resources :castles, only: [:new, :show,:create,:update]
+  resources :castles, only: [:new, :show,:create,:update] 
   resources :users, only: [:index]
   resources :citadels, only: [:index, :show]
 end
