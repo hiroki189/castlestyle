4c7df5c6e47e5e920c2c763e8be565724bc84b2b
hiroki189 <hiroki18950@icloud.com>
2019-09-12 16:05:26 +0900

    castle.rb

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -1,8 +1,10 @@
 class Castle < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
-  belongs_to_active_hash :castlename
-  delegate :name, to: :castlename
+  belongs_to_active_hash :siro
+  # delegate :name, to: :castlename
   has_many :citadels
   has_one_attached :image
+
+  # validates :name, :castlename ,presence: true
 end
