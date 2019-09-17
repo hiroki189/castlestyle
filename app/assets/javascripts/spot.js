e3d092c057a34137e8f6e1d40981f874cc8075ff
hiroki189 <hiroki18950@icloud.com>
2019-09-17 15:00:23 +0900

    spot.js select

--- app/assets/javascripts/spot.js
+++ app/assets/javascripts/spot.js
@@ -4,16 +4,12 @@ $(function(){
 
   function appendSpot(spot){
     var html = `<li class="modal__tab__result__collect__place">
-                  <div class="modal__tab__result__collect__place__contain">
-                    <div class="modal__tab__result__collect__place__contain__name">
-                      ${ spot.name }
-                        </div>
-                          <div class="modal__tab__result__collect__place__contain__select">
-                        <button id="modal__tab__result__collect__place__contain__select__action">
-                          選択
-                      </button>
-                    </div>
-                  </div>
+                  <div class="modal__tab__result__collect__place__contain__name">
+                    ${ spot.name }
+                      </div>
+                      <button id="modal__tab__result__collect__place__contain__select__action" data-spot-id=${spot.id} data-spot-name=${spot.name}>
+                        選択
+                    </button>
                 </li>`
     search_list.append(html);
   }
@@ -27,9 +23,41 @@ $(function(){
     search_list.append(html);
   }
 
+    // var spot_list = $('.style__photo__narrow__container__new__place__collection__container__name');
+
+    // function addSpot(spotId,spotName){
+    //   var html = `<li class="style__photo__narrow__container__new__place__collection" id=${spotId}>
+    //                 <tabble class="style__photo__narrow__container__new__place__collection__container">
+    //                   <div class="style__photo__narrow__container__new__place__collection__container__name">
+    //                     ${spotName}
+    //                   </div>
+    //                   <div class="style__photo__narrow__container__new__place__collection__container__action">
+    //                     <a class="style__photo__narrow__container__new__place__collection__container__action_delete" href="">
+    //                       削除
+    //                     </a>
+    //                   </div>
+    //                 </tabble>
+    //               </li>`
+    //   spot_list.append(html);
+    // }
+
+  
+  var spot_list = $('.style__photo__narrow__container__new__place');
+  function addSpot(spotId,spotName){
+    var html = `<li class="style__photo__narrow__container__new__place__collection">
+      <tabble class="style__photo__narrow__container__new__place__collection__container">
+        <div class="style__photo__narrow__container__new__place__collection__container__name">${spotName}</div>
+          <div class="style__photo__narrow__container__new__place__collection__container__action">
+          <a class="style__photo__narrow__container__new__place__collection__container__action_delete" href="">削除</a>
+        </div>
+      </tabble>
+    </li>`
+  }
+
+
   $('#modal__tab__search__prevent__input').on("keyup", function(){
     var input = $("#modal__tab__search__prevent__input").val();
-    
+
     $.ajax({
       type: 'GET',
       url: '/castles/new',
@@ -39,7 +67,7 @@ $(function(){
 
     .done(function(spots){
       $(".modal__tab__result__collect").empty();
-      if (spots.length !==0 && input !==''){
+      if (spots.length !==0){
         spots.forEach(function(spot){
           appendSpot(spot);
         });
@@ -52,4 +80,13 @@ $(function(){
       aleart('スポット検索に失敗しました');
     })
   });
+
+  $('.modal__tab__result__collect').on("click", "#modal__tab__result__collect__place__contain__select__action", function(){
+    var spotId =$(this).data('spot-id');
+    var spotName = $(this).data('spot-name');
+    addSpot(spotId, spotName);
+    $(this).parent().remove();
+    $('#overlay, #modalWindow').fadeOut();
+  });
+
 });
