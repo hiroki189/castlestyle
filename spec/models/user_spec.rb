0ae3c3aa7be9aa181a16c80a423ce66a565249e1
hiroki189 <hiroki18950@icloud.com>
2019-09-22 20:19:15 +0900

    name emptiness test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
+
 describe User do
   describe '#create' do
+
     it "is invalid without a name" do
      user = build(:user, name: "")
      user.valid?
-     expect(user.errors[:name]).to include("can't be blank")
+     expect(user.errors[:name]).to include("を入力してください")
+    end
+
+    it "is invalid without a email" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
     end
 
   end
