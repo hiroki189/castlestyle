93a971897ba4d5e9f0694ef59459466a9ef99738
hiroki189 <hiroki18950@icloud.com>
2019-09-22 20:35:44 +0900

    password emptiness test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,29 @@ require 'rails_helper'
 describe User do
   describe '#create' do
 
-    it "is invalid without a name" do
-     user = build(:user, name: "")
-     user.valid?
-     expect(user.errors[:name]).to include("を入力してください")
+    it "is valid with a name, email, password, password_confirmation" do
+      user = build(:user)
+      expect(user).to be_valid
     end
 
     it "is invalid without a email" do
-      user = build(:user, email: "")
+    it "is invalid without a name" do
+      user = build(:user, name: "")
       user.valid?
-      expect(user.errors[:email]).to include("を入力してください")
-    end
+      expect(user.errors[:name]).to include("を入力してください")
+     end
+ 
+     it "is invalid without a email" do
+       user = build(:user, email: "")
+       user.valid?
+       expect(user.errors[:email]).to include("を入力してください")
+     end
+
+     it "is invalid without a password" do
+      user = build(:user, password: "")
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+     end
 
   end
 end
