f6cf9260305f9ca49961a1fce8f6ff915232e972
hiroki189 <hiroki18950@icloud.com>
2019-09-23 17:24:11 +0900

    create castle_controller_spec.rb

--- spec/controllers/castles_controller_spec.rb
+++ spec/controllers/castles_controller_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 describe CastlesController do
-  describe 'GET #new' do
-    it "render the :new template" do
+
+  describe 'GET #index' do
+    it "populates an array of casltes ordered by id ASC" do
+      castles = create_list(:castle, 3)
+      get :index
+      expect(assigns(:castles)).to match(castles.sort{ |a,b| a.id <=> b.id})
+    end
+
+    it "renders the :index template" do
       get :index
       expect(response).to render_template :index
     end
   end
+
+
+  describe 'GET #new' do
+    it "render the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
 
 end
