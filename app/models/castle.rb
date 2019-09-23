7b04ef4320eda368e75d7d39dbd0d7e5527956f9
hiroki189 <hiroki18950@icloud.com>
2019-09-23 15:55:49 +0900

    modify commentout delete

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -2,9 +2,7 @@ class Castle < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to_active_hash :siro
-  # delegate :name, to: :castlename
   has_many :citadels
   has_one_attached :image
 
-  # validates :name, :castlename ,presence: true
 end
