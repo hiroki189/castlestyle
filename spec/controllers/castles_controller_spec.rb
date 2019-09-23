0477672224a5091909926ef75bc87b588b1eb5a3
hiroki189 <hiroki18950@icloud.com>
2019-09-23 15:23:11 +0900

    castle_controller_spec new action index

--- spec/controllers/castles_controller_spec.rb
+++ spec/controllers/castles_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe CastlesController do
+  describe 'GET #new' do
+    it "render the :new template" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 
 end
