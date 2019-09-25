8157208e2b6f84f1bf7078cb25e0dbf21afebddd
hiroki189 <hiroki18950@icloud.com>
2019-09-25 15:00:49 +0900

    add user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
 
+  def self.find_for_oauth(auth)
+  user = User.where(uid: auth.uid, provider: auth.provider).first
+
+  unless user
+    user = User.create(
+      uid:      auth.uid,
+      provider: auth.provider,
+      email:    auth.info.email,
+      name:  auth.info.name,
+      password: Devise.friendly_token[0, 20],
+      image:  auth.info.image
+    )
+  end
+
+  user
+end
+
 
   validates :name, presence: true, presence: true, length: 3..20
 end
